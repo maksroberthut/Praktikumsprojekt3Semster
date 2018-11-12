d5bfa087b9f931d8004a8c1a8345480594eebcc1
BYYYYTCH <38835095+BYYYYTCH@users.noreply.github.com>
2018-11-12 22:10:23 +0100

    Stream 80pc

--- src/Main.java
+++ src/Main.java
@@ -17,45 +17,33 @@ import java.io.IOException;
 import java.text.AttributedCharacterIterator;
 
 public class Main extends OpenJPanel{
-   private BufferedImage bufImg ;
+
 
     static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 
     public static void main (String[] args){
-        try {
-            cameraStream();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-    public static VideoCapture cameraStream() throws IOException {
-        Mat mat = new Mat();
-
+        FacePanel facePanel = new FacePanel();
         JFrame screen = new JFrame();
         screen.setVisible(true);
         screen.setSize(800,800);
         screen.setVisible(true);
-
-
-
-        JPanel facePanel = new JPanel();
         screen.setContentPane(facePanel);
-        facePanel.setVisible(true);
 
+
+        Mat mat = new Mat();
         VideoCapture camera = new VideoCapture();
         camera.open(0);
-        screen.setSize(800,800);
 
         if(!camera.isOpened()){
             System.out.println("Stream Opening Error");
            // JOptionPane.showMessageDialog(null,"Please check you'r Device", "Camera",JOptionPane.INFORMATION_MESSAGE);
-        }else{
+        }
             while(true){
             if(mat.empty()){
                 try {
                     Thread.sleep(200);
-
+                    MatToBufferedImage.setMatrix(mat,".jpg");
+                    facePanel.setFace(bu);
                     facePanel.repaint();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -68,19 +56,5 @@ public class Main extends OpenJPanel{
         return camera;
 
     }
-
-    public void paintComponent(Graphics g){
-        super.paintComponent(g);
-        if(this.bufImg==null){
-            return;
-        }
-        g.drawImage(this.bufImg,10,10,this.bufImg.getWidth(null),this.bufImg.getHeight(null),null);
-    }
-    public void setFace (BufferedImage img){
-        bufImg = img;
-    }
-
-    public BufferedImage getBufImg(){
-        return bufImg;
-    }
 }
+
