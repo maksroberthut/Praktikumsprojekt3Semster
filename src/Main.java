05e6f00fa922de11e9f12c700a7919c554015bc2
BYYYYTCH <38835095+BYYYYTCH@users.noreply.github.com>
2018-11-12 16:32:18 +0100

    Stream(Test)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
-public class Main {
+import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
+
+import javax.swing.*;
+
+public class Main extends OpenJPanel{
 
     public static void main (String[] args){
+        cameraStream();
+
+    }
+    public static VideoCapture cameraStream(){
+        Mat mat = new Mat(1);
+        JFrame screen = new JFrame();
+        screen.setSize(200,200);
+        JPanel facePanel = new JPanel();
+        screen.setContentPane(facePanel);
+        VideoCapture camera = new VideoCapture(3);
+        camera.open(0);
+        screen.setSize(200,200);
+        if(!camera.isOpened()){
+            System.out.println("Stream Opening Error");
+        }else{
+            while(camera.read(mat)){
+            facePanel.repaint();
+
+            }
+        }
+        return camera;
+
+     /*   if(camera.isOpened()){
+            Thread.sleep(500);
+        }*/
 
     }
 }
--- src/OpenJPanel.java
+++ src/OpenJPanel.java
@@ -10,17 +10,23 @@ public class OpenJPanel extends JPanel {
     Mat frame = new Mat();
 
     OpenJPanel(){
-    camera = new VideoCapture();
+
+    camera = new VideoCapture(0);
     camera.open(0);
+    JFrame screen = new JFrame();
+    screen.setSize(200,200);
+    stream(camera,screen);
+
+
     }
 
-    public VideoCapture stream(VideoCapture stream, JPanel panel){
+    public VideoCapture stream(VideoCapture stream, JFrame screen){
 
         if(!stream.isOpened()){
             System.out.println("Stream Opening Error");
         }else{
             while(stream.read(frame)){
-                return
+                return camera;
 
             }
         }
